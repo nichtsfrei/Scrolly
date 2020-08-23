1dd8d25ecc124eafe4fbbdeb19bc3aa131cebf80
Philipp Eder <philipp.eder@posteo.net>
2020-08-23 17:55:11 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Scrolly
+
+Is a small program with the sole purpose to change the scroll wheel lines for 
+non continues scrolling devices ( a classical mouse) from `1 .. n` 
+(depending on how furious you scroll) to constant `3` in Mac OSX.
+
+## Compile
+
+```
+xcodebuild -target Scrolly -configuration Release
+```
+## How to use
+
+To be able to use `Scrolly` you need to go to 
+
+`Preferences -> Security & Privacy -> Accessibility` 
+
+and then add `Scrolly`. This allows `Scrolly` to listen to change events.
+
+After that you can start `Scrolly`, there will be no message or visual feedback.
+
+To start `Scrolly` at login you need to go to `Preferences -> Users & Groups -> Login Items`.
---
+++ Scrolly/ContentView.swift
@@ -0,0 +1,85 @@
+import Cocoa
+
+let LINES_TO_SCROLL: Int64 = 3
+func setScrollSpeed(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, refcon: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
+    let classicalMousewheel = event.getIntegerValueField(.scrollWheelEventIsContinuous) == 0
+    if (classicalMousewheel){
+        var lines = LINES_TO_SCROLL
+        if (event.getIntegerValueField(.scrollWheelEventDeltaAxis1) < 0){
+            lines *= -1
+        }
+        event.setIntegerValueField(.scrollWheelEventDeltaAxis1, value: lines)
+    }
+    return Unmanaged.passRetained(event)
+}
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate, NSPopoverDelegate {
+    
+    let statusItem: NSStatusItem
+    let popover: NSPopover
+    var popoverMonitor: AnyObject?
+    
+    override init() {
+        popover = NSPopover()
+        statusItem = NSStatusBar.system.statusItem(withLength: 24)
+    
+        let eventMask = (1 << CGEventType.scrollWheel.rawValue)
+        guard let eventTap = CGEvent.tapCreate(tap: .cgSessionEventTap,
+                                              place: .headInsertEventTap,
+                                              options: .defaultTap,
+                                              eventsOfInterest: CGEventMask(eventMask),
+                                              callback: setScrollSpeed,
+                                              userInfo: nil) else {
+                                                print("Failed to create event tap")
+                                                exit(1)
+        }
+
+        let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+        CGEvent.tapEnable(tap: eventTap, enable: true)
+        DispatchQueue.global(qos: .background).async {
+            CFRunLoopRun()
+        }
+        
+        
+        super.init()
+        setupStatusButton()
+    }
+    
+    func setupStatusButton() {
+        if let statusButton = statusItem.button {
+            statusButton.image = NSImage(named: "Status")
+            statusButton.alternateImage = NSImage(named: "StatusHighlighted")
+        }
+    }
+    
+    func onPress(sender: AnyObject) {
+        if popover.isShown == false {
+            openPopover()
+        }
+        else {
+            closePopover()
+        }
+    }
+    
+    func openPopover() {
+        if let statusButton = statusItem.button {
+            statusButton.highlight(true)
+            popover.show(relativeTo: .null, of: statusButton, preferredEdge: .minX)
+            
+        }
+    }
+    
+    func closePopover() {
+        popover.close()
+        if let statusButton = statusItem.button {
+            statusButton.highlight(false)
+        }
+        if let monitor : AnyObject = popoverMonitor {
+            NSEvent.removeMonitor(monitor)
+            popoverMonitor = nil
+        }
+    }
+}
